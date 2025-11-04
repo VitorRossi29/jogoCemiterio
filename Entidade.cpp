306699c24d165bb2210cce4f331480f6eae2d54a
Vitor Rossi <vitor.2025@alunos.utfpr.edu.br>
2025-11-03 22:00:24 -0300

    Teste 2

--- Entidade.cpp
+++
@@ -1,9 +0,0 @@
-#include "Entidade.h"
-
-CaveiraDeCristal::Entidades::Entidade::Entidade(const sf::Vector2f posicao, const sf::Vector2f tam, IDs::ID id = IDs::ID::vazio) :
-	Ente(posicao, tam),
-	id(id)
-{}
-
-CaveiraDeCristal::Entidades::Entidade::~Entidade()
-{}
--- Obstaculo.cpp
+++
@@ -1,8 +0,0 @@
-#include "Obstaculo.h"
-
-CaveiraDeCristal::Entidades::Obstaculos::Obstaculo::Obstaculo(const sf::Vector2f posicao, const sf::Vector2f tam, IDs::ID id = IDs::ID::vazio) :
-	Entidade(posicao, tam)
-{}
-
-CaveiraDeCristal::Entidades::Obstaculos::Obstaculo::~Obstaculo()
-{}
